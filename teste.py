fdcd628a83399b99fadd380f9048a99ee8028810
Ian Carvalho <iancarv@gmail.com>
2014-09-13 14:55:51 -0300

    Finalizado teste generico

---
+++ teste.py
@@ -0,0 +1,19 @@
+class FalhouNoTeste(Exception):
+	pass
+
+class Teste(object):
+	"Classe que representa um teste generico"
+
+	def run(self):
+		if self.entrada_esperada == self.saida_esperada:
+			return True
+		else:
+			raise FalhouNoTeste
+
+	def entrada_esperada(self):
+		return ''
+
+	def saida_esperada(self):
+		return ''
+
+
