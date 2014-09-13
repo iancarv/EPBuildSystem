abca15afce7f90dafea7974c680d4ef02d017bbb
Ian Carvalho <iancarv@gmail.com>
2014-09-13 16:04:27 -0300

    finished builder

--- builder.py
+++ builder.py
@@ -1,8 +1,15 @@
 import os
 import zipfile
+import settings
 from testes.tester import Tester
 
 
+def zipdir(path, zip):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            zip.write(os.path.join(root, file))
 
 tester = Tester()
 if tester.test():
+	zip_name = zipfile.ZipFile(settings.dest, 'w')
+	zipdir(settings.dir, zip_name)
---
+++ settings.py
@@ -0,0 +1,4 @@
+grupo = ['0000000', '0000001'] #nusps
+title = 'mini-epx'
+dir = 'source'
+dest = title + '-' + '-'.join(grupo) + '.zip'
